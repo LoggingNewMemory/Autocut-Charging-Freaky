ae3c77b200719cb0183bb1eafae63dde6558d986
Kanagawa Yamada <83739103+LoggingNewMemory@users.noreply.github.com>
2024-10-13 21:33:30 +0700

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,38 @@
+#!/system/bin/sh
+
+ui_print "🔋----------------------------------------------🔋"
+ui_print "   Tecno Pova 5 Pro Ambatucut Charging Oohhshit   "
+ui_print "🔋----------------------------------------------🔋"
+ui_print " By: Ambanazerealnofake With Ambagawa Rusdiyamada "
+ui_print "--------------------------------------------------"
+ui_print "    Dont Read The Telegram Massage Or Be Freaky   "
+ui_print "--------------------------------------------------"
+ui_print " "
+
+sleep 3
+ui_print "--------------------------------------------------"
+ui_print "         Injecting Air Muani to Lh8nigger         "
+ui_print "--------------------------------------------------"
+sleep 3
+
+ui_print "--------------------------------------------------"
+ui_print "          Injecting Successfully Complete         "
+ui_print "                Ahh Ambatubassssing               "
+ui_print "--------------------------------------------------"
+
+sleep 1
+ui_print " "
+ui_print "please nut your phone so this module will work ahhhhh"
+ui_print " if you not nut on your phone i will touch you >:) "
+sleep 3
+
+unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+mkdir -p $MODPATH/system/bin
+unzip -o "$ZIPFILE" 'ambatukam_charging' -d $MODPATH/system/bin >&2
+
+set_perm_recursive $MODPATH 0 0 0755 0644
+set_perm $MODPATH/system/bin/P0 0 0 0755 0755
+set_perm $MODPATH/system/bin/P1 0 0 0755 0755
+
+
+am start -a android.intent.action.VIEW -d https://t.me/KanagawaLabAnnouncement/123 >/dev/null 2>&1
